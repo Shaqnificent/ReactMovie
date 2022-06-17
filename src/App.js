56cd6275366aefb11ddc5f06b9baaea1d8fd009b
Shaqnificent <shaqnificent@gmail.com>
2022-06-17 16:06:21 -0500

    initialize the project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div>
+        <h1>Movie</h1>
+      </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
