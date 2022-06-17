d9f3c2f904a3458361d8b38fb9fa563e3c8d851a
Shaqnificent <shaqnificent@gmail.com>
2022-06-17 17:40:13 -0500

    Created a static view of the website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import {useEffect} from 'react';
 import './App.css';
-import SearchICon from './search.svg';
+import SearchIcon from './search.svg';
 //2e73ca0d
 
 const API_URL = "https://www.omdbapi.com?apikey=2e73ca0d";
-
+const Movie1 = {
+    "Title": "Spiderman and Grandma",
+    "Year": "2009",
+    "imdbID": "tt1433184",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMjE3Mzg0MjAxMl5BMl5BanBnXkFtZTcwNjIyODg5Mg@@._V1_SX300.jpg"
+}
 const App = () => {
 
     const searchMovies = async (title) => {
@@ -19,7 +25,30 @@ const App = () => {
     }, []);
 
     return (
-        <h1>Movie App</h1>
+        <div className='app'>
+            <h1>MovieWorld</h1>
+
+            <div className='search'>
+                <input placeholder='Search for movies' value="superman" onChange={()=>{}}/>
+                <img src={SearchIcon} alt='search' onClick={() => {}}/>
+            </div>
+
+            <div className='container'>
+                <div className='movie'>
+                    <div>
+                        <p>{Movie1.Year}</p>
+                    </div>
+                    <div>
+                        <img src={Movie1.Poster !== 'N/A' ? Movie1.Poster : 'https://via.placeholder.com/400'} alt="Movie1.Title" />
+                    </div>
+
+                    <div>
+                        <span>{Movie1.Type}</span>
+                        <h3>{Movie1.Title}</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
