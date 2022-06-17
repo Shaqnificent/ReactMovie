e0fd8376582a0cd1d3379925ee7a82f86156949e
Shaqnificent <shaqnificent@gmail.com>
2022-06-17 18:20:16 -0500

    added searching functionality

--- src/App.js
+++ src/App.js
@@ -6,15 +6,10 @@ import SearchIcon from './search.svg';
 import MovieCard from './MovieCard';
 
 const API_URL = "https://www.omdbapi.com?apikey=2e73ca0d";
-const Movie1 = {
-    "Title": "Spiderman and Grandma",
-    "Year": "2009",
-    "imdbID": "tt1433184",
-    "Type": "movie",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BMjE3Mzg0MjAxMl5BMl5BanBnXkFtZTcwNjIyODg5Mg@@._V1_SX300.jpg"
-}
+
 const App = () => {
     const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
@@ -31,8 +26,8 @@ const App = () => {
             <h1>MovieWorld</h1>
 
             <div className='search'>
-                <input placeholder='Search for movies' value="superman" onChange={()=>{}}/>
-                <img src={SearchIcon} alt='search' onClick={() => {}}/>
+                <input placeholder='Search for movies' value={searchTerm} onChange={(e)=> setSearchTerm(e.target.value)}/>
+                <img src={SearchIcon} alt='search' onClick={() => searchMovies(searchTerm)}/>
             </div>
 
             {
