2c21efabf3255798c3d87c570e22e4b72fdab48f
Shaqnificent <shaqnificent@gmail.com>
2022-06-17 17:24:12 -0500

    added required files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-
+import {useEffect} from 'react';
+import './App.css';
+import SearchICon from './search.svg';
 //2e73ca0d
+
+const API_URL = "https://www.omdbapi.com?apikey=2e73ca0d";
 
 const App = () => {
 
-        return (
-            <h1>Movie App</h1>
-        );
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, []);
+
+    return (
+        <h1>Movie App</h1>
+    );
 }
 
 export default App;
